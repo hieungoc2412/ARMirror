8c14e3bc0425386cf4e53f3571eaf9b5506b4468
HieuDN <duonghieu294@gmail.com>
2026-01-22 09:38:58 +0700

    Add Google Analytics G-0PHN701NNY

---
+++ google-analytics.js
@@ -0,0 +1,15 @@
+// Google Analytics - Measurement ID: G-0PHN701NNY
+// Google tag (gtag.js)
+(function() {
+    // Load Google Analytics script
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-0PHN701NNY';
+    document.head.appendChild(script);
+
+    // Initialize gtag
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-0PHN701NNY');
+})();
